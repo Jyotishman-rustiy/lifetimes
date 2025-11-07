524fd797dd56db9f96dc09074e918e2a86fe2060
Jyotishman pathak <jyotishmanpathak11@gmail.com>
2025-11-07 23:09:21 +0530

    lifetimes

---
+++ src/lib.rs
@@ -0,0 +1,57 @@
+//!
+//#![warn(missing_debug_implementations, rust_2018_idioms, missing_docs)]
+
+#[derive(Debug)]
+pub struct StrSplit <'a> {
+    remainder:Option< &'a str>,
+    delimiter: &'a str,
+}
+
+impl<'a> StrSplit <'a>{
+    pub fn new(haystack: &'a str, delimiter: &'a str) -> Self {
+        Self {
+            remainder: Some(haystack),
+            delimiter,
+        }
+    }
+}
+
+impl<'a>  Iterator for StrSplit <'a>{
+    type Item = &'a str;
+
+  fn next(&mut self ) -> Option<Self ::Item> {
+    
+    let remainder = self.remainder.as_mut()?;
+
+        if let Some(next_delim) = remainder.find(self.delimiter){
+            let until_delimeter = &remainder[..next_delim];
+            *remainder =  &remainder[(next_delim + self.delimiter.len())..];
+            Some(until_delimeter)
+         }
+        else {
+            self.remainder.take()
+
+        }
+        }
+    }
+  
+
+
+
+
+#[test]
+fn itworks() {
+    let haystack = "a b c  d e";
+    let letters: Vec<_> = StrSplit::new(haystack, " ").collect();
+    assert_eq!(letters, vec!["a", "b", "c", "d", "e"]);
+}
+
+#[test]
+fn tail(){
+    let haystack = "a b c d ";
+    let letters :Vec<_> = StrSplit::new(haystack, "").collect();
+    assert_eq!(letters, vec!["a", "b", "c" , "d"]);
+    
+
+}
+
